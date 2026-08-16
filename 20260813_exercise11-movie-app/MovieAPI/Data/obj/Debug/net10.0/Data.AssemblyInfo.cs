285adb2bed415367d57a8ba08a2c724e36a3759e
KarlqvistLars <le.karlqvist@gmail.com>
2026-08-16 22:56:48 +0200

    save

--- 20260813_exercise11-movie-app/MovieAPI/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ 20260813_exercise11-movie-app/MovieAPI/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f2207b7aa05a27f6811352eb7fd3d46078ae47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8894b473238251869ea1b70f16ca56957d78b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
